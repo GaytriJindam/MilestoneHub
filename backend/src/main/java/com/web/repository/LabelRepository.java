c02e74d49201fa24680c6345dd8383956a96e5b2
GaytriJindam <gayatrijindam121@gmail.com>
2025-04-10 12:03:43 +0100

    commit

--- backend/src/main/java/com/web/repository/LabelRepository.java
+++ backend/src/main/java/com/web/repository/LabelRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.zosh.model.User;
+import com.web.model.User;
 
 import aj.org.objectweb.asm.Label;
 
